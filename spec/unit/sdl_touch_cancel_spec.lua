55b576fdd7f7a224d410b89d56e7ba708632d794
Minaduki-Shigure <Minaduki-Shigure@users.noreply.github.com>
2026-09-03 17:20:26 +0800

    test(ios): cover gesture scope transitions

--- spec/unit/sdl_touch_cancel_spec.lua
+++ spec/unit/sdl_touch_cancel_spec.lua
@@ -37,13 +37,14 @@ describe("SDL touch cancellation", function()
 end)
 
 describe("SDL bottom horizontal suppression scope", function()
-    local Input, UIManager
+    local Input, TouchState, UIManager
     local fake_ui_manager, input, owner, stack
     local applied
 
     setup(function()
         require("commonrequire")
         Input = require("device/input")
+        TouchState = require("ffi/sdl3_touch_state")
         UIManager = require("ui/uimanager")
     end)
 
@@ -93,5 +94,29 @@ describe("SDL bottom horizontal suppression scope", function()
         input:setBottomHorizontalSuppressed(false)
         assert.same({ true, false, true, false }, applied)
         assert.is_nil(input._bottom_horizontal_suppression_owner)
+    end)
+
+    it("cancels an active reader contact once when an overlay takes input", function()
+        local touch_state = TouchState:new()
+        input.input.setBottomHorizontalSuppressed = function(suppressed)
+            touch_state:setBottomHorizontalSuppressed(suppressed)
+        end
+
+        input:setBottomHorizontalSuppressed(true, owner)
+        assert.equals("forward", touch_state:onDown(101, false, 500, 900))
+
+        stack = { {}, owner }
+        input:_syncBottomHorizontalSuppression()
+        assert.is_true(touch_state:takePendingCancel())
+        assert.is_false(touch_state:takePendingCancel())
+        assert.equals("consume", touch_state:onUp(101))
+        assert.is_false(touch_state:isDiscarding())
+
+        stack = { owner }
+        input:_syncBottomHorizontalSuppression()
+        assert.is_false(touch_state:takePendingCancel())
+        assert.equals("forward", touch_state:onDown(202, false, 500, 900))
+        assert.equals("cancel", touch_state:onMotion(202, 700, 900, 1000, 1000))
+        assert.equals("consume", touch_state:onUp(202))
     end)
 end)
